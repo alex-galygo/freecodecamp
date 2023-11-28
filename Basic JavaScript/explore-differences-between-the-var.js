c700fb91a1e71d3c2870a13ce8f0cb5cdd7f6782
Alex Galygo <alex.galygo.94@gmail.com>
2023-11-28 18:12:33 +0300

    Replace 'var' with 'let' in JavaScript file
    In the file 'explore-differences-between-the-var.js', 'var' declarations were replaced with 'let', initializing the variables 'catName' and 'catSound' with the respective string values 'Oliver' and 'Meow!'. This change was made to ensure compliance with ES6+ standards which recommend the use of 'let' and 'const' over 'var'.

---
+++ Basic JavaScript/explore-differences-between-the-var.js
@@ -0,0 +1,16 @@
+// Description:
+//var should not exist in the code.
+// Waiting:catName should be the string Oliver.
+// Waiting:catSound should be the string Meow!
+
+// Solution:
+
+let catName = "Oliver";
+let catSound = "Meow!";
+
+// Tests:
+console.log(catName);
+console.log(catSound);
+
+
+
