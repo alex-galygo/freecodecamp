e80024d8b38b181f13b6690ec7e8211d5a463763
Alex Galygo <alex.galygo.94@gmail.com>
2023-11-28 22:29:14 +0300

    Add functionWithArgs in JavaScript
    A new file 'passing-values-to-functions-with-arguments.js' has been committed. It includes a function named functionWithArgs that takes two arguments and subsequently prints their sum to the console. This change allows us to demonstrate a fundamental concept in JavaScript - functions with parameters. Simple function calls for testing have also been added.

---
+++ Basic JavaScript/passing-values-to-functions-with-arguments.js
@@ -0,0 +1,11 @@
+// Description:
+//  - Create a function called functionWithArgs that accepts two arguments and outputs their sum to the dev console.
+
+// Solution:
+function functionWithArgs(num1, num2) {
+    console.log(num1 + num2);
+}
+
+// Tests:
+functionWithArgs(1,2);
+functionWithArgs(7,9);
