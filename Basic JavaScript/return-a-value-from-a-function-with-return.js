469f5f8734edaec77451e7ae7f14392d4e2caa34
Alex Galygo <alex.galygo.94@gmail.com>
2023-11-28 22:31:30 +0300

    Add function 'timesFive' in JavaScript
    A new file 'return-a-value-from-a-function-with-return.js' has been added. This file includes a function named 'timesFive' that takes a single argument, multiplies it by five and returns the result. This is to demonstrate the basis of returning a manipulated value in JavaScript functions. The function has also been tested with different arguments to ensure correctness.

---
+++ Basic JavaScript/return-a-value-from-a-function-with-return.js
@@ -0,0 +1,12 @@
+// Description:
+// Create a function timesFive that accepts one argument, multiplies it by 5, and returns the new value.
+
+// Solution:
+function timesFive(num) {
+    return num * 5;
+}
+
+// Tests:
+console.log(timesFive(5));
+console.log(timesFive(2));
+console.log(timesFive(0));
