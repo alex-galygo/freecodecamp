d92ede605dcf31a947b58dc08f479c8a6ce3465d
Alex Galygo <alex.galygo.94@gmail.com>
2023-11-28 22:26:37 +0300

    Introduce reusableFunction in JavaScript
    A new file 'write-reusable-javascript-with-functions.js' has been added. This contains a declaration for reusableFunction, which is a simple function that outputs "Hi World" to the console. This function presents a basic example of reusability in JavaScript for educational purposes. A simple function invocation test has also been included.

---
+++ Basic JavaScript/write-reusable-javascript-with-functions.js
@@ -0,0 +1,12 @@
+// Description:
+//   - Create a function called reusableFunction which prints "Hi World" to the dev console.
+
+
+// Solution:
+function reusableFunction() {
+    console.log("Hi World");
+}
+
+// Tests:
+reusableFunction()
+
