f6c01968f132f182ba87b44e5bcaf45f81ac79db
Alex Galygo <alex.galygo.94@gmail.com>
2023-11-29 21:24:45 +0300

    Add new JavaScript file for array accessing
    Added a new JavaScript file accessing-nested-arrays.js that demonstrates how to access items in nested arrays in JavaScript. This is necessary for teaching beginners how to handle complex data structures.

---
+++ Basic JavaScript/accessing-nested-arrays.js
@@ -0,0 +1,24 @@
+const myPlants = [
+    {
+        type: "flowers",
+        list: [
+            "rose",
+            "tulip",
+            "dandelion"
+        ]
+    },
+    {
+        type: "trees",
+        list: [
+            "fir",
+            "pine",
+            "birch"
+        ]
+    }
+];
+
+const secondTree = myPlants[1].list[1];
+
+// Test
+
+console.log(secondTree) // should equal the string pine.
