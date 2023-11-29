fa71d2a6899d5c8139f1c6bb39793f234fccabae
Alex Galygo <alex.galygo.94@gmail.com>
2023-11-29 11:24:28 +0300

    Refactor code to use 'let' instead of 'var'
    The keyword 'var' was replaced with 'let' in the function 'testElse'. This change makes the code more modern and align with ES6 standards. Also, two extra lines have been added at the end of the function for better readability.

--- Basic JavaScript/introducing-else-statements.js
+++ Basic JavaScript/introducing-else-statements.js
@@ -3,22 +3,23 @@
 
 // Solution:
 function testElse(val) {
-    var result = "";
+    let result = "";
     // Only change code below this line
 
     if (val > 5) {
-      result = "Bigger than 5";
+        result = "Bigger than 5";
     } else {
-      result = "5 or Smaller";
+        result = "5 or Smaller";
     }
 
     // Only change code above this line
     return result;
-  }
+}
 
-  // Tests:
-  console.log(testElse(4));
-  console.log(testElse(5));
-  console.log(testElse(6));
-  console.log(testElse(10));
+
+// Tests:
+console.log(testElse(4));
+console.log(testElse(5));
+console.log(testElse(6));
+console.log(testElse(10));
 
