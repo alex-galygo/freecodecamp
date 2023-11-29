0a5b7ce2a7de25a89bfba550a6abb8850f5015e3
Alex Galygo <alex.galygo.94@gmail.com>
2023-11-29 11:08:55 +0300

    Add lessOrEqual operator functionality in JavaScript
    Added a new file, 'comparison-with-the-less-than-or-equal-to-operator.js' which contains a function, 'testLessOrEqual'. This function tests the less than or equal to operator in JavaScript by taking a value and returning a string message that indicates if the given value is less than or equal to 12, 24 or greater than 24. Also added a test console log for verification.

---
+++ Basic JavaScript/comparison-with-the-less-than-or-equal-to-operator.js
@@ -0,0 +1,18 @@
+// Description:
+// Add the less than or equal to operator to the indicated lines so that the return statements make sense.
+
+// Solution:
+function testLessOrEqual(val) {
+    if (val <= 12) {  // Change this line
+        return "Smaller Than or Equal to 12";
+    }
+
+    if (val <= 24) {  // Change this line
+        return "Smaller Than or Equal to 24";
+    }
+
+    return "More Than 24";
+}
+
+// Tests:
+console.log(testLessOrEqual(0));
