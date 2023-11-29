52b7300297eb800ceab0e0ebdc7c4b9c728df04b
Alex Galygo <alex.galygo.94@gmail.com>
2023-11-29 11:06:41 +0300

    Add greaterOrEqual operator functionality in JavaScript
    A new file 'comparison-with-the-greater-than-or-equal-to-operator.js' has been introduced. This file features the function 'testGreaterOrEqual()' which incorporates the greater than or equal to operator in JavaScript. The function determines whether the input value is greater than or equal to 10 or 20 and returns the corresponding string message. A test case has been added for evaluation.

---
+++ Basic JavaScript/comparison-with-the-greater-than-or-equal-to-operator.js
@@ -0,0 +1,18 @@
+// Description:
+// Add the greater than or equal to operator to the indicated lines so that the return statements make sense.
+
+// Solution:
+function testGreaterOrEqual(val) {
+    if (val >= 20) {  // Change this line
+        return "20 or Over";
+    }
+
+    if (val >= 10) {  // Change this line
+        return "10 or Over";
+    }
+
+    return "Less than 10";
+}
+
+// Tests:
+console.log(testGreaterOrEqual(0));
