b68823f4ad6756186f67df424025ccc69181d54e
Alex Galygo <alex.galygo.94@gmail.com>
2023-11-30 17:46:35 +0300

    Added parseInt function with radix in JavaScript
    A JavaScript function 'convertToInteger' was implemented in this commit. This function makes use of the 'parseInt' function with radix, allowing for string-to-integer conversion. It enhances the application's type consistency and data validation capabilities.

---
+++ Basic JavaScript/use-the-parseint-function-with-a-radix.js
@@ -0,0 +1,15 @@
+// Description: Use the parseInt Function with a Radix
+
+// Solution
+
+function convertToInteger(str) {
+    return parseInt(str, 2);
+}
+
+convertToInteger("10011");
+
+
+// Tests
+
+
+console.log(convertToInteger("10011")); // convertToInteger("10011") should return a number
