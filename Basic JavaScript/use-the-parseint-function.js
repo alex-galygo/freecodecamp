8c2376fbc3696276740f8339fc4ba8129fd810fb
Alex Galygo <alex.galygo.94@gmail.com>
2023-11-30 17:46:16 +0300

    Add a function to convert string to integer using parseInt in JS
    This commit includes implementation of a new function 'convertToInteger'. The function uses 'parseInt' function in JS to convert string input to an integer. This will facilitate conversion of possible string inputs to integers within the application. This addition contributes to data validation and type consistency efforts across the codebase.

---
+++ Basic JavaScript/use-the-parseint-function.js
@@ -0,0 +1,14 @@
+// Description:
+// Use the parseInt Function
+
+// Solution
+
+function convertToInteger(str) {
+    return parseInt(str)
+}
+
+convertToInteger("56");
+
+// Tests
+
+console.log(convertToInteger("56")) // should return a number
